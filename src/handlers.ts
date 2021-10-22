7f55253cc40b993ea5543fafa83b29f6da6c5c8f
fraol0912 <fraol0912@gmail.com>
2021-10-22 12:51:59 +0300

    fix limiting logic

--- src/handlers.ts
+++ src/handlers.ts
@@ -20,9 +20,10 @@ export function handleSearch(searchWord: string): string {
 
   console.log("[SEARCH_WORD]:", searchWord);
 
-  const foundWords = words
-    .filter(({ word }) => word.toLowerCase().includes(searchWord.toLowerCase()))
-    .splice(30);
+  const foundWords = words.filter(({ word }) =>
+    word.toLowerCase().includes(searchWord.toLowerCase())
+  );
+  foundWords.splice(30);
 
   console.log("[FOUND_WORD]:", foundWords);
 
